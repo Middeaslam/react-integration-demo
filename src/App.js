8cf14f910f5f3bf6bd6342fca29a32d8d68f9f17
Mahammad Aslam <middeaslam36@gmail.com>
2023-01-11 17:06:40 +0530

    Updated child one

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import logo from './logo.svg';
 // window.React = React
 
 function App1() {
+  console.log(process.env.REACT_APP_API_KEY);
   return (
     <div className="App">
       <App />
-
+      <span>I am in child one</span>
     </div>
   );
 }
